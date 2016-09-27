cfbc2d42a1d206b08f66872f8b7f95cd65c25a5d
Jason Arnold <jason.arnold.00@gmail.com>
2016-09-27 16:41:17 +0000

    Done.

--- nyc_pigeon_organizer.rb
+++ nyc_pigeon_organizer.rb
@@ -1,3 +1,24 @@
 def nyc_pigeon_organizer(data)
   # write your code here!
+  pigeon_list = {}
+  data.each do |attr, detail|
+    detail.each do |type, name_array|
+      name_array.each do |name|
+        pigeon_list[name] = {}
+      end
+    end
+  end
+
+  pigeon_list.each do |name, info|
+    data.each do |attr, detail|
+      arr = []
+      detail.each do |type, name_array|
+        if name_array.include?(name)
+          arr.push(type.to_s)
+        end
+        pigeon_list[name][attr] = arr
+      end
+    end
+  end
+  pigeon_list
 end
